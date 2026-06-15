3935ba417cfe9772bebb1ec4e02229002acdbe9a
ADITYA SINHA <adityasinha1805007386@gmail.com>
2026-06-15 21:13:44 +0530

    add these

---
+++ chapter 4 loop control instruction/09_for.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        printf("the value of n is %d\n");
+    }
+    return 0;
+}
---
+++ chapter 4 loop control instruction/quick_quiz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n =9;
+    for(int i = 1; i <= n; i++){
+        printf("%d\n", i);
+
+
+    }
+    
+    return 0;
+}
+ 
