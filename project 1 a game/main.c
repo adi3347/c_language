869ac344d857504e4a35d4e35c1ab8dd39e370e4
ADITYA SINHA <adityasinha1805007386@gmail.com>
2026-06-19 00:54:57 +0530

    adding the project

---
+++ project 1 a game/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    /* Seed the PRNG with the current time */
+    srand((unsigned) time(NULL));
+
+    /* Generate a number in the range 1..1000 */
+    int r = rand() % 1000 + 1;
+    int no_of_guesses = 0;
+    int guessed;
+
+    // printf("random number:%d\n", r);
+    do{
+        printf("guess the number between 1 and 1000:");
+        scanf("%d", &guessed);
+        if(guessed>r){
+            printf("the number is too high\n");
+        } else if(guessed<r){
+            printf("the number is too low\n");
+        } else {
+            printf("congrats u guessed it!\n");
+        }
+        no_of_guesses++;
+    }while(guessed!=r);
+    printf("you guessed the number in %d tries\n", no_of_guesses);
+    
+    return 0;
+}
